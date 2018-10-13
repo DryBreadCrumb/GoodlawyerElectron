03810fffdc154e55a0556e02eecf44fadcb4e83c
Parker <parker.smith725@gmail.com>
2018-10-13 01:32:22 -0600

    Boilerplate

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/styles.scss';
+
+const jsx = (
+    <div>HELLO WORLD FROM REACT</div>
+);
+
+const appRoot = document.getElementById('app');
+ReactDOM.render(jsx,appRoot);
