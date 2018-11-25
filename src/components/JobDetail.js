99fa77c0cc7131a7e1daddf73caf75763cc49ba8
MJ2097 <mj2097@163.com>
2018-11-25 13:16:29 -0700

    job placeholder page

---
+++ src/components/JobDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class JobDetail extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <p>This is a placeholder for JobDetail</p>
+        );
+    }
+
+}
---
+++ src/components/JobOverview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class JobOverview extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <p>This is a placeholder for JobOverview</p>
+        );
+    }
+}
