d738be5604b0a76400f4d29208621a0ade36efed
Kanchana <kmarasinghe1@gmail.com>
2018-11-24 18:57:11 -0700

    Add paths for future pages

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,6 +5,17 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import LandingPage from '../components/LandingPage';
 import Login from '../components/Login';
 
+// import Quotes from '../components/Quotes';
+// import QuoteDetails from '../components/QuoteDetails';
+//
+// import Jobs from '../components/Jobs';
+// import JobDetails from '../components/JobDetails';
+//
+// import Message from '../components/Message';
+//
+// import Fixed from '../components/Fixed';
+// import FixedDetails from '../components/FixedDetails';
+
 class AppRouter extends React.Component {
 	constructor(props) {
 		super(props);
@@ -15,7 +26,18 @@ class AppRouter extends React.Component {
 			<div>
 				<Switch>
 					<Route path='/' component={Login} exact={true} />
-					<Route path='/landing' component={LandingPage} exact={true} />
+					<Route path='/login' component={Login} exact={true} />
+
+					{/* <Route path='/quotes' component={Quotes} exact={true} />
+					<Route path='/quotes/:id' component={QuoteDetails} exact={true} />
+
+					<Route path='/jobs' component={Jobs} exact={true} />
+					<Route path='/jobs/:id' component={JobDetails} exact={true} />
+
+					<Route path='/message/:id' component={Message} exact={true} />
+
+					<Route path='/fixed' component={Fixed} exact={true} />
+					<Route path='/fixed/:id' component={FixedDetails} exact={true} /> */}
 				</Switch>
 			</div>
 		);
