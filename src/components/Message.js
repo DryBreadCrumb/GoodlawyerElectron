6fb38a924b774c26548e82c34631a4405a17409d
DryBreadCrumb <43456401+DryBreadCrumb@users.noreply.github.com>
2018-12-01 17:32:25 -0700

    Added Message Page (blank)

---
+++ src/components/Message.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// const msg = <h1> Message </h1>;
+
+class Message extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return(
+      <div>
+        {/*<div> msg </div>*/}
+        <div> Messages Page... </div>
+      </div>
+    );
+  }
+
+}
+export default Message;
