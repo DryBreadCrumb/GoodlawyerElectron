d976f425786eb45d9e1bce8fdd87a285227af6c1
RB28Z2 <kmarasinghe1@gmail.com>
2018-12-01 23:29:51 -0700

    Able to use jwt and get jobs feed from DB

--- src/actions/feedAction.js
+++ src/actions/feedAction.js
@@ -1,3 +1,24 @@
 import axios from 'axios';
+import createElectronStore from 'electron-store-webpack-wrapper';
 
-// for accessing... database stuff
+const store = createElectronStore();
+const jwt = store.get('jwt');
+
+var instance = axios.create({
+  baseURL: 'http://kanchana.ddns.net:3001/',
+  timeout: 2000,
+  headers: {'x-auth': jwt}
+});
+
+export const getFeed = user => {
+  return (dispatch) => {
+    return instance
+      .get('jobs/pending')
+      .then(function(response) {
+        return response.data;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+};
--- src/components/feed.js
+++ src/components/feed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {getFeed} from '../actions/feedAction';
 import Sidebar from './Sidebar';
 
 class Feed extends React.Component {
@@ -8,11 +9,23 @@ class Feed extends React.Component {
 
         this.state = {
         };
+
+        this.handleClick = this.handleClick.bind(this);
     }
 
     // logout(){
     //     this.props.dispatch(logoutUser());
     // }
+
+    handleClick(e) {
+      e.preventDefault();
+      console.log('Starting GET request for new feed info');
+      this.props.dispatch(getFeed())
+      .then(result => {
+        console.log('Done');
+        console.log(result);
+      });
+    }
 
     render() {
         return (
@@ -26,6 +39,13 @@ class Feed extends React.Component {
                     <span className='login-sub-title'>
                     my work here is done
                     </span>
+
+                    <div>
+                      <button
+                        onClick={this.handleClick}>
+                        Get feed
+                        </button>
+                    </div>
                 </div>
 
             </div>
